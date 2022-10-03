c58f8e6b883758234d5da6355bdaddbb881e6a2f
Josano Pereira <67811104+JosanoPereira@users.noreply.github.com>
2022-10-03 23:47:52 +0100

    Relacionando as Apps

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from apps.funcionarios.models import Funcionario
 
 
 class Documento(models.Model):
     num_doc = models.CharField(max_length=14)
+    dono = models.ForeignKey(Funcionario, on_delete=models.PROTECT, verbose_name='Proprietário do Documento')
 
     def __str__(self):
         return self.num_doc
