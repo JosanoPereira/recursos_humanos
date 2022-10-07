1540baa28b4e69a5ef253d21ae5bd0f8dd65a274
Josano Pereira <67811104+JosanoPereira@users.noreply.github.com>
2022-10-07 19:05:12 +0100

    autorizando host

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -89,7 +89,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
+STATIC_ROOT = [
+    BASE_DIR / 'staticfiles'
+]
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
 
