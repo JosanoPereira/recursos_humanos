77f5c134fe4af8b814a32a4c117cf9c276a0bdba
Josano Pereira <67811104+JosanoPereira@users.noreply.github.com>
2022-10-18 11:14:54 +0100

    nginx

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -86,10 +86,12 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, "static")
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
 
